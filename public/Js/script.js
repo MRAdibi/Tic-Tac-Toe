3a9655ac5cc70d3b4db7e573b18d37842bd0c323
AlirezaRezaie <rezaiealireza884@gmail.com>
2023-01-08 19:28:57 +0330

    moved game algorithm to server

--- public/Js/script.js
+++ public/Js/script.js
@@ -1,6 +1,7 @@
 const { createApp } = Vue;
-let counter = 0;
-let winner = "";
+
+
+var socket = io("/socket");
 
 createApp({
   data() {
@@ -14,23 +15,27 @@ createApp({
       ],
     };
   },
+  created() {
+    // socket connections
+    socket.on("connect", function () {
+      console.log("Connected to server");
+    });
+    socket.on("disconnect", function () {
+      console.log("Disconnected from server");
+    });
+    socket.on("update game", (data) => {
+      console.log(data.gameState);
+      data.gameState.forEach((row, i) => {
+        row.forEach((col, j) => {
+          this.gameState[i][j] = data.gameState[i][j];
+        });
+      });
+      this.turn = data.turn;
+    });
+  },
   methods: {
     click(v) {
-      // translating event number to (row,col) form
-      let row = Math.ceil(v / 3) - 1;
-      let col = 2 - (3 * Math.ceil(v / 3) - v);
-
-      if (this.gameState[row][col] === "") {
-        this.gameState[row][col] = this.turn;
-        this.turn === "x" ? (this.turn = "o") : (this.turn = "x");
-      } else {
-        return;
-      }
-      if (checkWin(this.gameState)) {
-        this.gameOver();
-      } else if (counter === 9) {
-        this.gameOver();
-      }
+      socket.emit("update value", v);
     },
     gameOver() {
       // reset states
@@ -41,44 +46,3 @@ createApp({
     },
   },
 }).mount("#app");
-
-/**
-check if all elements of an array are equal (to find if some line win)
- */
-const allEqual = (arr, callback) => {};
-
-function checkWin(grid) {
-  let array = [[], []];
-
-  for (let i = 0; i < grid.length; i++) {
-    //diagonals
-    array[0].push(grid[i][i]);
-    array[1].push(grid[i][2 - i]);
-
-    // rows
-    array.push(grid[i]);
-
-    //columns
-    array.push(grid.map((x) => x[i]));
-  }
-
-  for (let element of array) {
-    if (element.every((v) => v === "x")) {
-      console.log("X won");
-      winner = "x";
-      break;
-    } else if (element.every((v) => v === "o")) {
-      console.log("O won");
-      winner = "o";
-      break;
-    }
-  }
-
-  if (winner) {
-    return true;
-  } else {
-    console.log("no won");
-    counter++;
-    return false;
-  }
-}
